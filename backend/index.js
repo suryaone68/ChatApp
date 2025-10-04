6009ad01099a2ca9b13c585613cbac0df1e79410
suryaone68 <suryapratapunn34@gmail.com>
2025-10-04 23:29:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import http from "http"
 
 const app = express()
 const server  = http.createServer(app)
-const io = new Server(server) // handle sockets io  
+const io = new Server(server)  
 dotenv.config()
 const Port = process.env.Port
 
@@ -15,7 +15,7 @@ io.on("connection", (socket)=>{
      // console.log("first", socket.id)
       socket.on("User-Message",(message)=>{
         console.log("a new user", message )
-        io.emit("message", message) // sbhi ko bhj dia server p  agr frontend se msg aata hai 
+        io.emit("message", message) 
      })
      
 })
